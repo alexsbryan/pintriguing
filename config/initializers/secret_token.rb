8c7a076351f115ba827ea26a9fe92f171e93da6d
Alex Bryan <student@appacademy.io>
2014-03-19 10:54:47 -0700

    Initial commit of Rails Skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pintriguing::Application.config.secret_key_base = 'e29ea11608064d2ee17d98c53d7e808e66c5b54d6945f02bd8d91cdbe8be5c14c8deedbbe13405fd7e82d5af81188df56b8d2700cf6b870da977014b9805020b'
